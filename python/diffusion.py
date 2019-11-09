63bab136b93d0a384eba13a7d43a4068cca1eb1a
Jack Moshell <jack.moshell@live.mercer.edu>
2019-11-08 22:21:54 -0500

    equilibriates

--- python/diffusion.py
+++ python/diffusion.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 
+# Equilibriated in 51.438 seconds.
 #x = right, y = down, z = back
 
 ## constants
 Lroom = 5.0 #m
 Rdivs = 10
-R = Lroom/Rdivs #m
-Urms = 0.250 #m/s
+Rdist = Lroom/Rdivs #m
+Urms = 250 #m/s
 D = 0.175 
-Tstep = Lroom/Urms/Rdivs
+Tstep = Lroom/Urms/float(Rdivs)
+Dterm = D*Tstep/(Rdist**2)
 #room
 room = [[[0.0 for i in range(Rdivs)] for j in range(Rdivs)] for k in range(Rdivs)]
 
@@ -38,6 +40,12 @@ def minMaxRatio():
 	room2d = [z for x in room for y in x for z in y]
 	return min(room2d)/max(room2d)
 
+#prints out the data for room all neat-like.
+def nicePrint():
+	for x in room:
+		for y in x:
+			print(y)
+
 #testing script
 def testSuite():
 	for x in range(Rdivs):
@@ -45,3 +53,18 @@ def testSuite():
 			print( "(" + str(x) + ", " + str(y) + ") can diffuse to: " + str(canDiffuse([x, y, 0])))
 	
 ### MAIN
+room[0][0][0] = float(10**21) #NEAT LANGUAGE THING: I really miss the exponent operator in other languages.
+Ttotal = 0.0
+while minMaxRatio() < 0.99:
+	for i in range(Rdivs):
+		for j in range(Rdivs):
+			for k in range(Rdivs):
+				for nbr in canDiffuse([i,j,k]):
+					#LESS NEAT LANGUAGE THING: Whitespace syntax sucks.
+					#Look at all that space on the left! Wasted.
+					change = (room[i][j][k] - room[nbr[0]][nbr[1]][nbr[2]])*Dterm
+					room[i][j][k] -= change
+					room[nbr[0]][nbr[1]][nbr[2]] += change
+	Ttotal += Tstep
+	print(str(Ttotal) + "\t " + str(minMaxRatio()))
+
