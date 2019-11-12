6b6dd23ffa7cd4c6dcb9930cf01049ecebd1e3d2
Jack Moshell <jack.moshell@live.mercer.edu>
2019-11-11 19:27:37 -0500

    julia initial commit

--- julia/diffusion.jl
+++ julia/diffusion.jl
@@ -10,12 +10,25 @@ const Rdist = Lroom/Rdivs
 const Tstep = Lroom/Urms/Rdivs
 Dterm = D*Tstep/Rdist/Rdist
 
-cube = zeros(Float64, Rdivs, Rdivs, Rdivs)
+room = zeros(Float64, Rdivs, Rdivs, Rdivs)
+
+### FUNCTIONS
+# Functions must be defined before their first use. As far as I can tell, this is undocumented.
+
+function minMaxRatio(room)
+	#NEAT LANGUAGE THING: Julia supports maximum over multidimensional arrays.
+	#No need for fancy tricks like some other languages. 
+	return minimum(room)/maximum(room)
+end
+
+#tests features
+function testSuite()
+	miniroom = zeros(Float64, 3, 3, 3)
+	miniroom[1, 1, 1] = 5.0
+	miniroom[3, 3, 3] = -5.0
+	println(minMaxRatio(miniroom))
+end
 
 ### MAIN
 
-
-### FUNCTIONS
-function minMaxRatio(cube)
-	return 
-end
+testSuite()
