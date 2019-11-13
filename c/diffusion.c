f0a695e4e7baa1f7b2fe3321d4e3d45d3853ed92
Jack Moshell <jack.moshell@live.mercer.edu>
2019-11-13 10:43:29 -0500

    partition

--- c/diffusion.c
+++ c/diffusion.c
@@ -3,9 +3,12 @@
 #include <math.h>
 
 /* TESTING RESULTS
+ * Without partition:
  * At Rdivs = 10, equilibriated in 51.438 seconds simulated time, 3.095 seconds real time.
  * At Rdivs = 500, completes a single step in 17.125 seconds.
  * (Compare that to python's 2m52s and ~15 minutes respectively. C can run!)
+ * With partition:
+ * At Rdivs = 10, equilibriated in 259.294 seconds simulated, 20.489 seconds user time.
  */
 
 //NEAT LANGUAGE THING: Macros! This one gives an easy way to treat a 1d array
@@ -21,20 +24,41 @@ const double Tstep = Lroom/Urms/(double)Rdivs;
 const double Dterm = D * Tstep /Rdist /Rdist;
 const int volelCount = Rdivs*Rdivs*Rdivs;
 
+int partitionOn = 1;
+const int Px = Rdivs/2;
+const int Py = (3*Rdivs)/4;
+
+int inbounds(int dx, int dy, int dz);
+
 //Determines the ratio of the least concentrated volel to the greatest.
-//Note how room is treated as 1d here.
+//Room can't be treated as 1d here because inbounds requires coordinates.
 double minMaxRatio(double* room){
 	double min = INFINITY;//INFINITY is a macro defined by gcc
 	double max = 0.0;
-	for(int i = 0; i < volelCount; ++i){
-		if(room[i] > max){
-			max = room[i];
-		}
-		if(room[i] < min){
-			min = room[i];
+	for(int x = 0; x < Rdivs; ++x){
+		for(int y = 0; y < Rdivs; ++y){
+			for(int z = 0; z < Rdivs; ++z){
+				if(!partitionOn || inbounds(x, y, z)){
+					if(ROOM(x, y, z) > max){
+						max = ROOM(x, y, z);
+					}
+					if(ROOM(x, y, z) < min){
+						min = ROOM(x, y, z);
+					}
+				}
+			}
 		}
 	}
 	return min/max;
+}
+
+int inbounds(int dx, int dy, int dz){
+	if( dx >= Rdivs || dx < 0 ) return 0;
+    if( dy >= Rdivs || dy < 0 ) return 0;
+    if( dz >= Rdivs || dz < 0 ) return 0;
+	//partition
+	if(partitionOn && dx == Px && dy <= Py) return 0;
+	return 1;
 }
 
 //Performs diffusion between two volels.
@@ -49,11 +73,12 @@ void diffuse(double* room, int x, int y, int z, int dx, int dy, int dz){
 	dy = y+dy;
 	dz = z+dz;
 	//bounds checking
-	if( dx >= Rdivs || dx < 0 ) return;
-	if( dy >= Rdivs || dy < 0 ) return;
-	if( dz >= Rdivs || dz < 0 ) return;
+	if(inbounds(x, y, z) + inbounds(dx, dy, dz) < 2){
+		return;
+	}
 	//diffusion
 	double change = (ROOM(x, y, z) - ROOM(dx, dy, dz))*Dterm;
+	//printf("C= %f\n", change);
 	ROOM(x, y, z) = ROOM(x, y, z) - change;
 	ROOM(dx, dy, dz) = ROOM(dx, dy, dz) + change;
 	return;
@@ -79,9 +104,9 @@ int main(void){
 		}
 		}
 		Ttotal += Tstep;
-		printf("%.5f\t%.8f\n", Ttotal, minMaxRatio(room));
+		//printf("%.5f\t%.8f\n", Ttotal, minMaxRatio(room));
 	}
-	
+	printf("%.5f \t%.8f\n", Ttotal, minMaxRatio(room));
 	free(room);
 	return 0;
 }
