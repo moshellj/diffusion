8ed52d3e17ea2cfee4d3144cd9c83241ac8ef469
Jack Moshell <jack.moshell@live.mercer.edu>
2019-11-15 22:09:06 -0500

    added c# partition

--- c#/README.md
+++ c#/README.md
@@ -2,4 +2,4 @@ To compile: mcs diffusion.cs
 To run: mono diffusion.cs
 To change settings:
     The divs parameter in the constructor for room sets the subdivisions in each dimension.
-    Partition not implemented in this language.
+    Likewise, set partitionOn in the constructor to enable or disable the partition.
--- c#/diffusion.cs
+++ c#/diffusion.cs
@@ -13,7 +13,7 @@ class Diffusion{
 	static void Main(string[] args){
 		// initialize
 		// NEAT LANGUAGE THING: Named arguments! Helps other people understand your code.
-		Room room = new Room(divs: 25, length: 5.0);
+		Room room = new Room(divs: 10, length: 5.0, partitionOn: true);
 		Gas gas = new Gas(Urms: 250.0, D: 0.175);
 		double Tstep = room.length / gas.rms / (double)(room.divs);
 		double Dterm = gas.D * Tstep / room.cellDist / room.cellDist;
@@ -27,11 +27,13 @@ class Diffusion{
 			for(int y = 0; y < room.divs; ++y){
 			for(int z = 0; z < room.divs; ++z){//for each cell
 				here = new Coord(x, y, z);
-				List<Coord> nbrs = room.neighbors(here);
-				foreach(Coord nbr in nbrs){
-					double change = (room.get(here) - room.get(nbr)) * Dterm;
-					room.set(here, room.get(here) - change);
-					room.set(nbr,  room.get(nbr)  + change);
+				if(room.inBounds(here)){
+					List<Coord> nbrs = room.neighbors(here);
+					foreach(Coord nbr in nbrs){//neigbors are already in bounds
+						double change = (room.get(here) - room.get(nbr)) * Dterm;
+						room.set(here, room.get(here) - change);
+						room.set(nbr,  room.get(nbr)  + change);
+					}
 				}
 			}
 			}
@@ -42,12 +44,20 @@ class Diffusion{
 		Console.WriteLine("{0:0.000000} \t{1:0.00000000}", Ttotal, minMaxRatio(room));
 	}
 	//Calculates the concentration ratio
+	//This used to be really cool, but bounds checking means it can't be.
 	static double minMaxRatio(Room room){
-		// NEAT LANGUAGE THING: Object-orientation allow arrays to inherit a
-		// number of methods from various interfaces. Here, Cast converts
-		// a 3d array to an IEnumerable, which supports lineaar iteration.
-		double max = room.cell.Cast<double>().Max();
-		double min = room.cell.Cast<double>().Min();
+		double min = 1e100;
+		double max = 0.0;
+		for(int x = 0; x < room.divs; ++x){
+		for(int y = 0; y < room.divs; ++y){
+		for(int z = 0; z < room.divs; ++z){
+			if(room.inBounds(new Coord(x, y, z))){
+				if(room.cell[x, y, z] > max) max = room.cell[x, y, z];
+				if(room.cell[x, y, z] < min) min = room.cell[x, y, z];
+			}
+		}
+		}
+		}
 		return min/max;
 	}
 }
@@ -69,11 +79,17 @@ class Room{
 	public double length;
 	public double cellDist;
 	public double[ , , ] cell;
+	bool partitionOn;
+	int Px;
+	int Py;
 	
-	public Room(int divs, double length){
+	public Room(int divs, double length, bool partitionOn = false){
 		this.divs = divs;
 		this.length = length;
 		cellDist = length/(double)this.divs;
+		this.partitionOn = partitionOn;
+		Px = divs/2;
+		Py = 3*divs/4;
 		cell = new double[this.divs, this.divs, this.divs];
 	}
 	
@@ -111,9 +127,8 @@ class Room{
 	
 	//tests if a coordinate is inside the room.
 	public bool inBounds(Coord here){
-		if( here.x < 0 || here.x >= divs ) return false;
-		if( here.y < 0 || here.y >= divs ) return false;
-		if( here.z < 0 || here.z >= divs ) return false;
+		if( here.x < 0 || here.x >= divs || here.y < 0 || here.y >= divs || here.z < 0 || here.z >= divs ) return false;
+		if( partitionOn && here.x == Px && here.y <= Py) return false;
 		return true;
 	}
 }
